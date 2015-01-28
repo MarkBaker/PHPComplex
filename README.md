4b65b18344d362478165ddbad1d176cb38514fa1
MarkBaker <mark@lange.demon.co.uk>
2015-01-28 22:29:07 +0000

    Added atan() function

--- README.md
+++ README.md
@@ -40,6 +40,7 @@ together with functions for
  - csch (hyperbolic secant)
  - acsch (inverse hyperbolic secant)
  - tan (tangent)
+ - atan (inverse tangent)
  - sqrt (square root)
  - exp (exponential)
  - ln (natural log)
@@ -50,7 +51,6 @@ together with functions for
 
 Planned additional functionality, but still to implement
 
- * atan (inverse tangent)
  * tanh (hyperbolic tangent)
  * atanh (inverse hyperbolic tangent)
  * cot (cotangent)
---
+++ unitTests/classes/src/functions/atanTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Complex;
+include_once __DIR__ . '/baseFunctionTest.php';
+
+class atanTest extends baseFunctionTest
+{
+    protected static $functionName = 'atan';
+
+    /**
+     * @dataProvider providerATan
+     */
+	public function testAtan()
+	{
+		$args = func_get_args();
+		$complex = new Complex($args[0]);
+		$result = atan($complex);
+
+        $this->complexNumberAssertions($args[1], $result);
+        // Verify that the original complex value remains unchanged
+        $this->assertEquals(new Complex($args[0]), $complex);
+	}
+
+    /*
+     * Results derived from Wolfram Alpha utang
+     *  N[ArcTan[<VALUE>], 18]
+     */
+    public function providerATan()
+    {
+		$expectedResults = array(
+			1.48765509490645539,
+			1.48996835348641902,
+            0.122828550687415431,
+            '1.50860933996675541+0.03408455783748144i',
+            '1.50860933996675541-0.03408455783748144i',
+            '1.56805945317620666+0.14832558812930499i',
+            '1.56805945317620666-0.14832558812930499i',
+            '0.220218187728579852+0.793955003613620776i',
+            '0.220218187728579852-0.793955003613620776i',
+            -1.46988976584971284,
+            -0.779184875808632190,
+            '-1.485900295172941+0.03693048705753099i',
+            '-1.485900295172941-0.036930487057531i',
+            '-0.8263539907993+0.2116413714332627i',
+            '-0.8263539907993-0.2116413714332627i',
+            '1.57079632679489662+0.32976531495669911i',
+            '-1.57079632679489662-0.32976531495669911i',
+            \NAN,
+            \NAN,
+            '0.123625981183130070i',
+            '-0.1236259811831301i',
+		);
+
+		return $this->formatOneArgumentTestResultArray($expectedResults);
+	}
+
+}
