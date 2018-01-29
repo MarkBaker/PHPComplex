23066ad9166da3e47b1696a042bd9bc54c7c7e26
MarkBaker <mark@lange.demon.co.uk>
2018-01-29 00:20:08 +0000

    resolve atanh test failures

--- classes/src/functions/atanh.php
+++ classes/src/functions/atanh.php
@@ -23,7 +23,7 @@ function atanh($complex)
     $imaginary = $complex->getImaginary();
     if ($imaginary == 0.0) {
         $real = $complex->getReal();
-        if ($real > -1.0 && $real < 1.0) {
+        if ($real >= -1.0 && $real <= 1.0) {
             return new Complex(\atanh($real), 0.0, $complex->getSuffix());
         } else {
             return new Complex(\atanh(1 / $real), (($real < 0.0) ? M_PI_2 : -1 * M_PI_2));
--- unitTests/classes/src/functions/atanhTest.php
+++ unitTests/classes/src/functions/atanhTest.php
@@ -15,7 +15,6 @@ class atanhTest extends baseFunctionTest
 		$args = func_get_args();
 		$complex = new Complex($args[0]);
         $result = atanh($complex);
-//var_dump($args, $complex, $result); echo PHP_EOL;
         $this->complexNumberAssertions($args[1], $result);
         // Verify that the original complex value remains unchanged
         $this->assertEquals(new Complex($args[0]), $complex);
@@ -37,12 +36,12 @@ class atanhTest extends baseFunctionTest
             '0.00262072256421015-1.42459748928464954i',
             '0.084423222108281457+0.601290456468997789i',
             '0.084423222108281457-0.601290456468997789i',
-            '-1.46988976584971284i',
-            '-0.779184875808632190i',
+            '-0.10159858166645237+1.57079632679489662i',
+            -2.54052612615238432,
             '-0.08507023405349073+1.53336304554086626i',
             '-0.08507023405349073-1.53336304554086626i',
             '-0.774365673480939215+0.878142243314273647i',
-            '-0.774365673480939215+0.878142243314273647i',
+            '-0.774365673480939215-0.878142243314273647i',
             '1.26262725567891168i',
             '-1.26262725567891168i',
             '0.785398163397448310i',
